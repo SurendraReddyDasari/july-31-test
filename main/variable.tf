92f56ca42bc533804dc57ef9805c4d601448bd5d
SurendraReddyDasari <dasarisurendrareddy123@gmail.com>
2023-08-01 15:02:56 +0100

    commited with files

---
+++ main/variable.tf
@@ -0,0 +1,43 @@
+variable "rg_name" {
+  description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."  
+  default = "techslate-ade-rg987"
+}
+
+variable "location" {
+    description =  "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."    
+    default = "northeurope"  
+}
+
+variable "asp_name" {
+  description = ""
+  default = "techslate-asp-987"
+}
+
+variable "webapp_name" {
+  description = ""
+  default = "techslate-webapp-987"
+}
+
+variable "docker_image" {
+  default = "sonarqube:latest"
+}
+
+variable "container_name" {
+  default = "container-ts-987"
+  description = ""
+}
+
+variable "database_name" {
+  default = "database-ts-307"
+  description = ""
+}
+
+variable "db_firewall_rule_name" {
+  default = "firewall-ts-307"
+  description = ""
+}
+
+variable "db_server_name" {
+  description = "Name of the PostgreSQL server"
+  default     = "ts-db-server-307"
+}
---
+++ modules/db_firewall_rule/variables.tf
@@ -0,0 +1,45 @@
+variable "location" {
+  description = "Azure Region for the Database Server"
+  type        = string
+}
+
+variable "database_name" {
+  description = "Name of the Azure Database for PostgreSQL"
+  type        = string
+}
+
+variable "rg_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "administrator_login" {
+  description = "Administrator login for the PostgreSQL server"
+  type        = string
+  default     = "myadmin"
+}
+
+variable "administrator_login_password" {
+  description = "Administrator login password for the PostgreSQL server"
+  type        = string
+  default     = "Passowrd@123"
+}
+
+variable "sku_name" {
+  description = "SKU tier for the PostgreSQL server"
+  default     = "GP_Gen5_2"
+}
+
+variable "storage_mb" {
+  description = "Storage size in MB for the PostgreSQL server"
+  default     = 5120
+}
+variable "db_firewall_rule_name" {
+  type    = string
+  default = ""  
+}
+
+variable "db_server_name" {
+  type    = string
+  default = ""  
+}
